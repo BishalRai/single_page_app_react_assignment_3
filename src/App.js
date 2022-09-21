527c12c320359de0ef643c9c59fcf23619b1009e
Bishal Rai <bishal6193@gmail.com>
2022-09-22 01:09:16 +0545

    modified contents of all components;Routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// import Routes for navigation of pages
+import {Routes, Route} from 'react-router-dom';
+
+
+// import the components section here so that it can use those pages
+import Home from './components/Home';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Header from './components/Header';
+// import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import NotFound from './components/NotFound';
+
+
+
 
 function App() {
   return (
-    <p>Test</p>
+    // <p>Test</p>
+    <>
+  {/* <Navbar /> */}
+    <Header /> 
+
+    <div className='container'>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/contactus' element = {<ContactUs />} />
+        <Route path="*" element = {<NotFound />} />
+      </Routes>
+    </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function ContactUs(){
+    return(
+        // <>
+        <div>Contact Us</div>
+        // <Link to = "/about"> Go about</Link>
+        // </>
+    )
+}
